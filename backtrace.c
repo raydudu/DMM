e4556061ccf99be2baf46e7068279138a1247b38
Ray Dudu <raydudu@gmail.com>
2016-05-01 18:09:59 +0200

    ARM APCS backtrace parser update

--- backtrace.c
+++ backtrace.c
@@ -20,7 +20,7 @@ void print_backtrace(void **addr, int len) {
     free(symbollist);
 }
 
-static void __attribute__ ((unused)) out_of_stack_marker() {
+void __attribute__ ((unused)) out_of_stack_marker() {
 };
 
 #ifdef DMM_OWN_BACKTRACE
@@ -31,9 +31,9 @@ static void __attribute__ ((unused)) out_of_stack_marker() {
  * long pc = *(topfp - 0);
  */
 
-//assuming stack is less than 2048k
+//assuming stack is 16M
 //TODO use getrlimit(RLIMIT_STACK ... or parse /proc/self/maps
-#define STACK_ADDR_MASK (((1UL << 16) - 1) << (CHAR_BIT * sizeof(unsigned long) - 21))
+#define STACK_ADDR_MASK (-1L << (CHAR_BIT * sizeof(unsigned long) - 8))
 
 int get_backtrace(void **addr, int len) {
     int i;
@@ -42,24 +42,29 @@ int get_backtrace(void **addr, int len) {
 
     assert(addr != NULL && len > 0);
 
-    /* topfp = get_fp(); */
     asm volatile("mov %[topfp], fp" : [topfp] "=r" (topfp));
     asm volatile("mov %[sp], sp" : [sp] "=r" (sp));
-    sp &= STACK_ADDR_MASK;
+    sp = sp & STACK_ADDR_MASK;
 
     /* Top frame */
     //addr[0] = *(topfp); /* pc */
 
     /* Middle frames */
     for (i = 0; i < len; i++) {
-        if (((unsigned long)topfp & STACK_ADDR_MASK) != sp) {
-            //out of stack
-            addr[i] = &out_of_stack_marker;
+        if (*(topfp - 3) == NULL) {
+            // Bottom frame, lr invalid
+            addr[i] = *(topfp); /* pc */
+            i++;
             break;
         }
-
-        if (*(topfp - 3) == NULL) {
-            addr[i] = *(topfp); /* pc */
+        if (((unsigned long)(*(topfp - 3)) & STACK_ADDR_MASK) != sp) {
+            // Next fp is out of stack, lr valid, pc useless
+            addr[i] = *(topfp - 1); /* lr */
+            i++;
+            if (i < len) {
+                addr[i] = &out_of_stack_marker;
+                i++;
+            }
             break;
         }
 
